07e9918931367c3d57e6b15de2f121ce639cdcab
Takahiko ONO <t.marcus1987@gmail.com>
2021-01-09 13:19:04 +0900

    impl.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = process.env.TOKEN || '';
+const guildID = process.env.DVCW_GUILD_ID || '';
+const token = process.env.DVCW_TOKEN || '';
+const notifyChannel = process.env.DVCW_CHANNEL || '';
+
+if (!guildID) {
+	console.log("DVCW_GUILD_ID is empty.")
+	process.exit(1)
+}
+
+if (!token) {
+	console.log("DVCW_TOKEN is empty.")
+	process.exit(1)
+}
+
+if (!notifyChannel) {
+	console.log("DVCW_CHANNEL is empty.")
+	process.exit(1)
+}
 
 let sendMessage = (c, m) => {
 	console.log(`[ERROR][NotReady] Failed to send message(${m}) to channel(${c})`);
@@ -18,7 +35,7 @@ client.on('ready', () => {
 	console.log("CHANNEL", client.channels.cache)
 	sendMessage = (c, m) => {
 		client.channels.cache.forEach(channel => {
-			if (channel.type === 'text' && channel.name === c) {
+			if (channel.guild.id == guildID && channel.type === 'text' && channel.name === c) {
 				channel.send(m)
 			}
 		})
@@ -30,24 +47,32 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 	console.log("OLD", oldState);
 	console.log("NEW", newState);
 
+	if (newState.guild.id !== guildID) {
+		return
+	}
+
 	if (oldState.channelID === newState.channelID) {
 		if (!oldState.streaming && newState.streaming) {
 			console.log("Start streaming")
 			const c = client.channels.cache.get(newState.channelID)
-			sendMessage('activity', `:movie_camera: ${ client.users.cache.get(c.guild.ownerID).username } が配信を開始しました`)
+			const u = client.users.cache.get(newState.id)
+			sendMessage(notifyChannel, `:movie_camera: ${ u.username } が配信を開始しました`)
 		} else if (oldState.streaming && !newState.streaming) {
 			const c = client.channels.cache.get(newState.channelID)
-			sendMessage('activity', `:stop_button: ${ client.users.cache.get(c.guild.ownerID).username } が配信を終了しました`)
+			const u = client.users.cache.get(newState.id)
+			sendMessage(notifyChannel, `:stop_button: ${ u.username } が配信を終了しました`)
 		}
 	} else {
 		if (newState.channelID !== null) {
 			// Join
 			const c = client.channels.cache.get(newState.channelID)
-			sendMessage('activity', `:microphone2: ${ client.users.cache.get(c.guild.ownerID).username } が ボイスチャンネル(${c.name}) に参加しました`)
+			const u = client.users.cache.get(newState.id)
+			sendMessage(notifyChannel, `:microphone2: ${ u.username } が ボイスチャンネル(${c.name}) に参加しました`)
 		} else {
 			// Leave
 			const c = client.channels.cache.get(oldState.channelID)
-			sendMessage('activity', `:wave: ${ client.users.cache.get(c.guild.ownerID).username } が ボイスチャンネル(${c.name}) を退室しました`)
+			const u = client.users.cache.get(newState.id)
+			sendMessage(notifyChannel, `:wave: ${ u.username } が ボイスチャンネル(${c.name}) を退室しました`)
 		}
 	}
 })
